2352ac75bf9ea07a6ee3341351bf79e73720c33d
男岡侑吾 <u-go0425@otokookayuuwarenoMacBook-Air.local>
2020-05-03 11:38:11 +0800

    control buttom

--- main.js
+++ main.js
@@ -9,6 +9,18 @@ let FIELD_HEIGHT	= 22; // フィールドの高さ
 
 let FIELD_X			= 40; // フィールドのCanvas内のX座標
 let FIELD_Y			= 40; // フィールドのCanvas内のY座標
+
+let KEY_RIGHT		= 0; // 右キー
+let KEY_LEFT		= 1; // 左キー
+let KEY_UP			= 2; // 上キー
+let KEY_DOWN		= 3; // 下キー
+let KEY_SPACE		= 4; // スペースキー
+var key = Array(5); // キー判定用変数
+key[KEY_RIGHT]	= 0;
+key[KEY_LEFT]	= 0;
+key[KEY_UP]		= 0;
+key[KEY_DOWN]	= 0;
+key[KEY_SPACE]	= 0;
  
 let block = [ // ブロックの定義
   [	//	block type 0
@@ -223,12 +235,88 @@ function init() {
 	brot = 0; // ブロックの回転種類
 }
 
+//	キー操作関数
+function keyCtrl() {
+	if(key[KEY_RIGHT] <= 1 && key[KEY_LEFT] <= 1) {
+		bx += key[KEY_RIGHT] - key[KEY_LEFT]; // 横移動
+		
+		var breakflag = false;
+		for(var i = 0;i < BLOCK_HEIGHT;i++) {
+			for(var j = 0;j < BLOCK_WIDTH;j++) {
+				// 配列番号がおかしかったら処理しない
+				if(bx + j < 0 || bx + j >= FIELD_WIDTH ||
+					by + i < 0 || by + i >= FIELD_HEIGHT) continue;
+				
+				// 当たり判定
+				if(field[by + i][bx + j] != 0 && block[btype][brot][i][j] == 1) {
+					bx -= key[KEY_RIGHT] - key[KEY_LEFT]; // 移動距離分を戻す
+					breakflag = true; // ループを抜ける
+					break;
+				}
+			}
+			if(breakflag) break;
+		}
+		// キーの状態を更新
+		if(key[KEY_RIGHT] == 1) key[KEY_RIGHT]++;
+		else if(key[KEY_LEFT] == 1) key[KEY_LEFT]++;
+	}
+	
+	if(key[KEY_DOWN] <= 1 && key[KEY_UP] <= 1) {
+		brot += key[KEY_DOWN] - key[KEY_UP]; // 回転
+		if(brot < 0) brot = 3;
+		else if(brot > 3) brot = 0;
+    
+    var breakflag = false;
+		for(var i = 0;i < BLOCK_HEIGHT;i++) {
+			for(var j = 0;j < BLOCK_WIDTH;j++) {
+				// 配列番号がおかしかったら処理しない
+				if(bx + j < 0 || bx + j >= FIELD_WIDTH ||
+						by + i < 0 || by + i >= FIELD_HEIGHT) continue;
+				
+				// 当たり判定
+				if((field[by + i][bx + j] != 0 && block[btype][brot][i][j] == 1) ||
+					(block[btype][brot][i][j] == 1 && by + i < 0)) {
+					brot -= key[KEY_DOWN] - key[KEY_UP]; // 回転を戻す
+					if(brot < 0) brot = 3;
+					else if(brot > 3) brot = 0;
+					breakflag = true; // ループを抜ける
+					break;
+				}
+			}
+			if(breakflag) break;
+		}
+		
+		// キーの状態を更新
+		if(key[KEY_DOWN] == 1) key[KEY_DOWN]++;
+		else if(key[KEY_UP] == 1) key[KEY_UP]++;
+	}
+}
+
 //	更新関数
 function update() {
 	if(cnt % 30 == 0) {
-		by++; // ブロックを１マス落下
-	}
+    by++; // ブロックを１マス落下
+
+    var breakflag = false;
+		for(var i = 0;i < BLOCK_HEIGHT;i++) {
+			for(var j = 0;j < BLOCK_WIDTH;j++) {
+				//	配列番号がおかしい場合は処理しない
+				if(bx + j < 0 || bx + j >= FIELD_WIDTH ||
+					by + i < 0 || by + i >= FIELD_HEIGHT) continue;
+				
+				//	同じ座標（マス）にブロックとブロック・壁が重なったら
+				if(field[by + i][bx + j] != 0 && block[btype][brot][i][j] == 1) {
+					by--; // 移動距離分を戻す
+					breakflag = true; // ループを抜ける
+					break;
+				}
+			}
+			if(breakflag) break;
+		}
+  }
 }
+
+
 
 //	落下ブロックの描画
 function drawBlock() {
@@ -302,11 +390,36 @@ function drawFrame() {
 	}
 }
 
+document.addEventListener("keydown", e => { // キー押下処理
+	var keyCode = e.keyCode; // キーコードの取得
+	
+	switch(keyCode) {
+	case 39: key[KEY_RIGHT]++;	break;
+	case 37: key[KEY_LEFT]++;	break;
+	case 38: key[KEY_UP]++;		break;
+	case 40: key[KEY_DOWN]++;	break;
+  case 32: key[KEY_SPACE]++;	break;
+  }
+});
+
+document.addEventListener("keyup", e => { // キー離上処理
+	var keyCode = e.keyCode; // キーコードの取得
+	
+	switch(keyCode) {
+	case 39: key[KEY_RIGHT]	= 0; break;
+	case 37: key[KEY_LEFT]	= 0; break;
+	case 38: key[KEY_UP]	= 0; break;
+	case 40: key[KEY_DOWN]	= 0; break;
+	case 32: key[KEY_SPACE]	= 0; break;
+	}
+});
+
 init();
 requestAnimationFrame(main);
 function main() {
 	context.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT); // 画面クリア
   
+  keyCtrl(); // キー操作
   update(); // 更新
   drawBlock(); // ブロックを描画
   drawField(); // フィールドを描画
